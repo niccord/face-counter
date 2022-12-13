98d6a573b1188f8bf97592b435b8def1f60b22d2
Nicola Cordioli <n.cordioli@e-time.it>
2022-12-13 19:09:08 +0100

    Not a good start, docker it's a pain sometimes

---
+++ client/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <button @click="callBE">Hello</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    callBE: async function() {
+      console.log('hello')
+      let response = await fetch('/');
+      response = await response.text();
+      console.log(response)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
