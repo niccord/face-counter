33768f803550cc5e2153294e8ffa2e2aad65f22f
Nicola Cordioli <cordioli.nicola@gmail.com>
2022-12-21 23:49:59 +0100

    Add formatdate and some authToken tests

--- api/utilities.js
+++ api/utilities.js
@@ -2,6 +2,9 @@ const jwt = require('jsonwebtoken');
 const adminList = process.env.ADMIN_LIST.split(',');
 
 function authenticateToken(req, res, next) {
+  if (!req.headers || !req.headers['authorization']) {
+    return res.sendStatus(400)
+  } 
   const authHeader = req.headers['authorization']
   const token = authHeader && authHeader.split(' ')[1]
   if (token == null) return res.sendStatus(401)
